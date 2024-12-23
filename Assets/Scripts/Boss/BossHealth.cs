1f9c62590f7dad03d561cf5ad47fa0d3cb61d4ea
Lê Thanh Phát <98292493+ltphatt@users.noreply.github.com>
2024-12-23 07:37:22 +0700

    feat: create boss level 2

--- Assets/Scripts/Boss/BossHealth.cs
+++ Assets/Scripts/Boss/BossHealth.cs
@@ -4,20 +4,31 @@ using UnityEngine;
 
 public class BossHealth : MonoBehaviour
 {
-    public int health = 10;
-    public int maxHealth = 10;
+    public int health = 20;
+    public int maxHealth = 20;
+    public int enragedHealth = 10;
     Animator animator;
     [SerializeField] GameObject bossDeathffect;
+    SpriteRenderer spriteRenderer;
+    bool isEnraged = false;
 
     private void Start()
     {
         animator = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     public void TakeDamage(int damage)
     {
         health -= damage;
         animator.SetTrigger("Hurt");
+
+        if (health == maxHealth / 2 && !isEnraged)
+        {
+            isEnraged = true;
+            spriteRenderer.color = new Color(255f / 255f, 155f / 255f, 155f / 255f, 255f / 255f);
+            health += enragedHealth;
+        }
 
         if (health <= 0)
         {
