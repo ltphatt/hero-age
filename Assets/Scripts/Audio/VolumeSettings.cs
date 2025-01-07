7b28094dadedde54c5a486d54a7e0ae5c8c1ffda
21120515 <147778709+TranPhuocNhan@users.noreply.github.com>
2025-01-08 01:21:09 +0700

    chore: add audio settings for canvas, temp slide to check settings volume work

---
+++ Assets/Scripts/Audio/VolumeSettings.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class VolumeSetting : MonoBehaviour
+{
+    [SerializeField] private AudioMixer audioMixer;
+    [SerializeField] private Slider musicSlider;
+    [SerializeField] private Slider sfxSlider;
+
+    private void Start()
+    {
+        if (PlayerPrefs.HasKey("musicVolume"))
+        {
+            loadVolume();
+        }
+        else
+        {
+            SetMusicVolume();
+        }
+
+        if (PlayerPrefs.HasKey("sfxVolume"))
+        {
+            loadVolume();
+        }
+        else
+        {
+            SetSFXVolume();
+        }
+    }
+    public void SetMusicVolume()
+    {
+        // Get the value of the slider
+        float volume = musicSlider.value;
+        audioMixer.SetFloat("music", Mathf.Log10(volume) * 20);
+        PlayerPrefs.SetFloat("musicVolume", volume);
+    }
+    public void SetSFXVolume()
+    {
+        // Get the value of the slider
+        float volume = sfxSlider.value;
+        audioMixer.SetFloat("sfx", Mathf.Log10(volume) * 20);
+        audioMixer.SetFloat("player", Mathf.Log10(volume) * 20);
+        audioMixer.SetFloat("enemy", Mathf.Log10(volume) * 20);
+        audioMixer.SetFloat("playerMovement", Mathf.Log10(volume) * 20);
+
+        PlayerPrefs.SetFloat("sfxVolume", volume);
+    }
+
+    private void loadVolume()
+    {
+        musicSlider.value = PlayerPrefs.GetFloat("musicVolume");
+        sfxSlider.value = PlayerPrefs.GetFloat("sfxVolume");
+        SetMusicVolume();
+        SetSFXVolume();
+    }
+}
