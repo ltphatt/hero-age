587e892f8995af3363617000fb8d6d2e0dfd25cb
Truong Minh Phat <tmphat1312@gmail.com>
2025-01-11 06:18:31 +0000

    chore: add game menu controller rudiments

---
+++ Assets/Scripts/UI/GameMenuController.cs
@@ -0,0 +1,55 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameMenuController : MonoBehaviour
+{
+    public GameObject gameMenuScreen;
+    public GameObject gameMenuButton;
+    public GameObject settingsScreen;
+
+    void Start()
+    {
+        HideGameMenu();
+        ShowGameMenuButton();
+    }
+
+    public void FreezeBackground()
+    {
+        Time.timeScale = 0f;
+    }
+
+    public void UnfreezeBackground()
+    {
+        Time.timeScale = 1f;
+    }
+
+    public void ShowGameMenu()
+    {
+        gameMenuScreen.SetActive(true);
+    }
+
+    public void HideGameMenu()
+    {
+        gameMenuScreen.SetActive(false);
+    }
+
+    public void HideGameMenuButton()
+    {
+        gameMenuButton.SetActive(false);
+    }
+
+    public void ShowGameMenuButton()
+    {
+        gameMenuButton.SetActive(true);
+    }
+
+    public void HideSettingsScreen()
+    {
+        settingsScreen.SetActive(false);
+    }
+
+    public void ShowSettingsScreen()
+    {
+        settingsScreen.SetActive(true);
+    }
+}
