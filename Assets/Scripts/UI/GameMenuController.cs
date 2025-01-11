738273cfa20126095dbbcdcefd6831c8a163b9aa
Truong Minh Phat <tmphat1312@gmail.com>
2025-01-11 06:20:27 +0000

    feat: handle continue button click

--- Assets/Scripts/UI/GameMenuController.cs
+++ Assets/Scripts/UI/GameMenuController.cs
@@ -81,4 +81,10 @@ public class GameMenuController : MonoBehaviour
         SceneManager.LoadScene("Start Scene");
     }
 
+    public void OnContinueButtonClick()
+    {
+        HideGameMenu();
+        ShowGameMenuButton();
+    }
+
 }
