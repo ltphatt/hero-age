ef6b9b5f429e2a460ee6b0849756fff9cfbcc2d4
Truong Minh Phat <tmphat1312@gmail.com>
2025-01-17 15:33:08 +0000

    feat: add regain HP items to shop

--- Assets/Scripts/UI/ShopItemsUI.cs
+++ Assets/Scripts/UI/ShopItemsUI.cs
@@ -3,10 +3,19 @@ using UnityEngine;
 
 public class ShopItemsUI : MonoBehaviour
 {
+    [Header("Add Max HP")]
     public int addMaxHPAmount = 2;
     public int addMaxHPPrice = 5;
+
+    [Header("Add Max MP")]
     public int addMaxMPAmount = 2;
     public int addMaxMPPrice = 5;
+
+    [Header("Regain 50% HP")]
+    public int regain50HPPrice = 5;
+
+    [Header("Regain 100% HP")]
+    public int regain100HPPrice = 10;
 
     PlayerController playerController;
 
@@ -54,5 +63,15 @@ public class ShopItemsUI : MonoBehaviour
     public void OnAddMaxMPClick()
     {
         BuyItem(addMaxMPPrice, () => playerController.ChangeMaxMP(addMaxMPAmount));
+    }
+
+    public void OnRegain50HPClick()
+    {
+        BuyItem(regain50HPPrice, () => playerController.ChangeHealth(playerController.maxHP / 2));
+    }
+
+    public void OnRegain100HPClick()
+    {
+        BuyItem(regain100HPPrice, () => playerController.ChangeHealth(playerController.maxHP));
     }
 }
